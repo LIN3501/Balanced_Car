d949b84f2e812e1980b10ead0e5589de962f805f
2.18封面文字 <zichenglin404@gmail.com>
2026-03-09 23:52:30 +0800

    删除

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,7 +19,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stdio.h"
-#include "string.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
