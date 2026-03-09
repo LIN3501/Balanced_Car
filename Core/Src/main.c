17f823217559aa3eaa00dd38eef6aa8081536d01
2.18封面文字 <zichenglin404@gmail.com>
2026-03-09 23:47:56 +0800

    测试

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stdio.h"
+#include "string.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
